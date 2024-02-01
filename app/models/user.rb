cbc1e45a15eb7ecffa8e2bf58367ddfc1bb5816e
root <root@localhost>
2024-02-01 19:36:49 +0000

    the broken blog post

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ApplicationRecord
 #  has_many :posts
 #  has_many :comments
 #add another comment
-validates_presence_of :first_name
-validates_presence_of :last_name
+#validates_presence_of :first_name
+#validates_presence_of :last_name
 end
